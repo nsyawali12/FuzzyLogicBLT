080dde78464eabdea7f6e7fb308c0c1ad7ca21e4
nsyawali12 <nsyawali12@gmail.com>
2018-10-26 17:47:36 +0700

    Fuzzy First

---
+++ Fuzzy_Logic_BLT_v.0.py
@@ -0,0 +1,12 @@
+#Kamus IMPORT
+import random
+import math
+import excel
+#############
+
+tester = ("Hello World! ")
+print (tester)
+
+project = ("Tugas 2 Artificial Intelligence : Fuzzy_Logic ")
+print (project)
+
