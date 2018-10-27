a27d7b84c8055eb0f325ccb18f6aaea5d675d813
nsyawali12 <nsyawali12@gmail.com>
2018-10-28 03:19:53 +0700

    Rule Income

--- Fuzzy_Logic_BLT_v.0.py
+++ Fuzzy_Logic_BLT_v.0.py
@@ -1,7 +1,7 @@
 #Kamus IMPORT
 import random
 import math
-import excel
+
 #############
 
 tester = ("Hello World! ")
@@ -9,4 +9,19 @@ print (tester)
 
 project = ("Tugas 2 Artificial Intelligence : Fuzzy_Logic ")
 print (project)
+
+def RuleIncome(Income):
+	if ( Income < 0.5 ) :
+		Ki = ('Kecil')
+	elif ( (Income > 0.5) and ( Income <= 0.7 ) ):
+		Ki = ('Kecil')
+	elif ( (Income < 0.7 ) and (Income > 1.2 ) ):
+		Ki = ('Sedang')
+	elif ( (Income > 1.2 ) and (Income <= 1.4) ):
+		Ki = ('Sedang')
+	elif ( (Income > 1.4 ) and (Income <= 1.9) ):
+		Ki = ('Besar')
+	elif ( Income > 1.9 ):
+		Ki = ('Besar')
+
 
