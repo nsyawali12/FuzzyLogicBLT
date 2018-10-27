84021b8aac646b361a69995e1fea15dfdbc40e4c
nsyawali12 <nsyawali12@gmail.com>
2018-10-28 04:35:56 +0700

    RuleDI

--- Fuzzy_Logic_BLT_v.0.py
+++ Fuzzy_Logic_BLT_v.0.py
@@ -10,18 +10,65 @@ print (tester)
 project = ("Tugas 2 Artificial Intelligence : Fuzzy_Logic ")
 print (project)
 
+
+#Prosedur Aturan dari Pengelompokan Pendapatan
+
 def RuleIncome(Income):
 	if ( Income < 0.5 ) :
-		Ki = ('Kecil')
+		Ki = ("Kecil")
 	elif ( (Income > 0.5) and ( Income <= 0.7 ) ):
-		Ki = ('Kecil')
+		Ki = ("Kecil")
 	elif ( (Income < 0.7 ) and (Income > 1.2 ) ):
-		Ki = ('Sedang')
+		Ki = ("Sedang")
 	elif ( (Income > 1.2 ) and (Income <= 1.4) ):
-		Ki = ('Sedang')
+		Ki = ("Sedang")
 	elif ( (Income > 1.4 ) and (Income <= 1.9) ):
-		Ki = ('Besar')
+		Ki = ("Besar")
 	elif ( Income > 1.9 ):
-		Ki = ('Besar')
+		Ki = ("Besar")
+
+#Prosedur Aturan dari Pengelompokan Hutang
+def RuleDebt(Debt):
+	if ( Debt < 30):
+		Kd = ("Kecil")
+	elif ( ( Debt > 30) and (Debt <= 40) ):
+		Kd = ("Kecil")
+	elif ( (Debt > 40 ) and (Debt <= 70) ):
+		Kd = ("Sedang")
+	elif ( (Debt > 70 ) and (Debt <= 80) ):
+		Kd = ("Sedang")
+	elif ( (Debt > 80) and (Debt <= 98) ):
+		Kd = ("Besar")
+	elif ( (Debt > 98) ):
+		Kd = ("Besar")
+
+#Rule Pengelompokkan
+#R1 = HasilRuleIncome
+#R2 = HasilRuleDebt
+
+def RuleDI(R1, R2):
+	if( (R1 == "Kecil") and (R2 == "Kecil")):
+		ResR = ("Sedang")
+	elif ( (R1 == "Kecil") and (R2 == "Sedang") ):
+		ResR = ("Tinggi")
+	elif ( (R1 == "Kecil") and (R2 == "Tinggi")):
+		ResR = ("Tinggi")
+
+	elif ( (R1 == "Sedang") and (R2 == "Kecil")):
+		ResR = ("Tinggi")
+	elif ( (R1 == "Sedang") and (R2 == "Sedang")):
+		ResR = ("Sedang")
+	elif ( (R1 == "Sedang") and (R2 == "Tinggi")):
+		ResR = ("Tinggi")
+
+	elif ( (R1 == "Tinggi") and (R2 == "Kecil")):
+		ResR = ("Kecil")
+	elif ( (R1 == "Tinggi") and (R2 == "Sedang")):
+		ResR = ("Kecil")
+	elif ( (R1 == "Tinggi") and (R2 == "Sedang")):
+		ResR = ("Sedang")
+
+
+
 
 
